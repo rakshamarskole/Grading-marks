01ad625a2c99a23757da7da9cac97d788d687e2d
Raksha Marskole <65505713+rakshamarskole@users.noreply.github.com>
2020-05-19 00:23:32 +0530

    created grading marks.c
    this file prints grades based on the marks got by students.

---
+++ Grading marks.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+float main()
+ {
+ 	float mark;
+ 	printf("Enter the mark of the student:");
+ 	scanf("%f",&mark);
+ 	if(mark<=100 && mark>=85)
+ 	 {
+ 	 	printf("Grade A");
+	 }
+	else
+	    {
+	      if(mark<=84 && mark>=70)
+		    printf("Grade B");
+			
+		  else
+		  {
+		  	if(mark<=69 && mark>=55)
+		  	  printf("Grade C");
+		  	
+			else
+			{
+			  if(mark<=54 && mark>=40)
+			    printf("Grade D");
+				
+			  else
+			    printf("Grade F");		
+			}    
+		  }		
+	    } 
+ }
